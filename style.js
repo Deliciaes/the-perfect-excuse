fdae4b4aa5b245ff0efeb962f8b867d27ab05286
Deliciaes <ceramstedt@gmail.com>
2021-11-30 15:46:20 +0100

    arrays and randomizer const

---
+++ style.js
@@ -0,0 +1,80 @@
+const excuseStart = [
+  'Sorry, I cant, because',
+  'I wish I could, but',
+  'Something came up, namely',
+  'I double-booked myself and',
+  'I am so sorry, but',
+  'Hi, yeah, so the thing is',
+  'Oh no, was that today? I am so sorry but',
+];
+
+const prepositions = [
+  'I have plans with',
+  'I forgot that',
+  'I just remembered that',
+  'I overslept and discovered that',
+  'I just found out that',
+  'I recently made the descovery that',
+];
+
+const subjects = [
+  'my',
+  'my sisters',
+  'my brothers',
+  'my cousins',
+  'my mothers',
+  'my fathers',
+  'my landlords',
+  'my neighbours',
+  'my aunts',
+  'my uncles',
+];
+
+const objects = [
+  'cat',
+  'dog',
+  'goldfish',
+  'parrot',
+  'house',
+  'bed',
+  'sofa',
+  'bathroom',
+  'bedroom',
+  'car',
+  'motorcycle',
+  'office',
+  'school',
+  'rug',
+  'door',
+  'bicycle',
+  'TV',
+  'computer',
+  'phone',
+];
+
+const events = [
+  'drowned',
+  'died',
+  'broke its leg',
+  'got lost',
+  'disappeared',
+  'exploded',
+  'needed a glowup',
+  'asked me for a haircut',
+  'caught on fire',
+  'got launched into space',
+];
+
+const excuseEnd = [
+  'Maybe some other time?',
+  'I will have to get back to you later.',
+  'But lets catch up soon?',
+  'I will let you know once the situation has changed.',
+  'Thank you so much for your understanding',
+  'I will have to do a raincheck this one time.',
+  'Maybe next week instead?',
+];
+
+const randomExcuseStart = excuseStart.sort((a, b) => 0.5 - Math.random());
+
+console.log(randomExcuseStart[0]);
