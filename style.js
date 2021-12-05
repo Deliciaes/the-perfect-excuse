f025eb00f7e97f96574406497e7100b8064ea5f5
Emma Ramstedt <71063351+Deliciaes@users.noreply.github.com>
2021-12-05 17:42:05 +0100

    added canvas
    added a canvas which allows you to create a card using the random excuse and a card of the users choice.

--- style.js
+++ style.js
@@ -14,7 +14,7 @@ const prepositions = [
   'I just remembered that',
   'I overslept and discovered that',
   'I just found out that',
-  'I recently made the descovery that',
+  'I recently made the discovery that',
 ];
 
 const subjects = [
@@ -56,10 +56,10 @@ const objects = [
 const events = [
   'drowned.',
   'died.',
-  'broke their leg.',
+  'broke a leg.',
   'got lost.',
   'disappeared.',
-  'exploded.',
+  'imploded.',
   'needed a glowup.',
   'asked me for a haircut.',
   'caught on fire.',
@@ -72,13 +72,14 @@ const excuseEnd = [
   'I will have to get back to you later.',
   'But lets catch up soon?',
   'I will let you know once the situation has changed.',
-  'Thank you so much for your understanding',
+  'Thank you so much for your understanding.',
   'I will have to do a raincheck this one time.',
   'Maybe next week instead?',
 ];
 
 const theExcuse = document.querySelector('.the-excuse');
 const generateButton = document.querySelector('.generate');
+const excuseConfirm = document.querySelector('.excuse-confirm');
 
 generateButton.addEventListener('click', () => {
   const randomExcuseStart = excuseStart.sort((a, b) => 0.5 - Math.random());
@@ -88,5 +89,49 @@ generateButton.addEventListener('click', () => {
   const randomEvent = events.sort((a, b) => 0.5 - Math.random());
   const randomExcuseEnd = excuseEnd.sort((a, b) => 0.5 - Math.random());
 
-  theExcuse.innerHTML = `${randomExcuseStart[0]} ${randomPreposition[0]} ${randomSubject[0]} ${randomObject[0]} ${randomEvent[0]} ${randomExcuseEnd[0]}`;
+  const finishedExcuse = `${randomExcuseStart[0]} ${randomPreposition[0]} ${randomSubject[0]} ${randomObject[0]} ${randomEvent[0]} ${randomExcuseEnd[0]}`;
+  theExcuse.innerHTML = finishedExcuse;
+
+  excuseConfirm.style.opacity = '1';
+
+  const myFont = new FontFace('myFont', 'url(font/SpecialElite-Regular.woff)');
+
+  myFont.load().then(function (font) {
+    document.fonts.add(font);
+    const canvas = document.getElementById('card');
+    canvas.width = canvas.getBoundingClientRect().width;
+    canvas.height = canvas.getBoundingClientRect().height;
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = 0.5;
+    ctx.fillRect(0, 5, 600, 50);
+    ctx.fillRect(0, 62, 600, 50);
+    ctx.fillRect(0, 515, 600, 50);
+    ctx.globalAlpha = 1.0;
+    ctx.font = '34px myFont';
+    ctx.fillStyle = 'white';
+    ctx.fillText(`${randomExcuseStart[0]} ${randomPreposition[0]}`, 5, 40, 580);
+    ctx.fillText(
+      `${randomSubject[0]} ${randomObject[0]} ${randomEvent[0]}`,
+      5,
+      95,
+      580
+    );
+    ctx.fillText(`${randomExcuseEnd[0]}`, 5, 550, 580);
+    const images = document.querySelectorAll('#gallery-img');
+    images.forEach((image) => {
+      image.addEventListener('click', () => {
+        canvas.style.background = `url(${image.src})`;
+        const cardContainer = document.querySelector('.card-container');
+        cardContainer.style.opacity = '1';
+      });
+    });
+  });
+});
+
+const galleryButton = document.querySelector('.gallery-button');
+const showGallery = document.querySelector('.gallery-container');
+
+galleryButton.addEventListener('click', () => {
+  showGallery.style.opacity = '1';
 });
